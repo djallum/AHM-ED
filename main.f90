3d9a9fc98ac2377e64f5b46321e5b54747245c05
pjdaley <daleypj@mcmaster.ca>
2015-05-27 18:30:50 -0400

    finished code (results not accurate)
    graphs of the DOS are not accurate there must be a bug somewhere

--- main.f90
+++ main.f90
@@ -9,7 +9,7 @@ use routines
 
 implicit none
 
-integer, parameter :: npairs = 1
+integer, parameter :: npairs = 10000
 real, parameter :: t = 1  !hopping term
 real, parameter :: delta = 10 ! width of disorder 
 real, parameter :: U = 4 ! on-site interactions
@@ -21,9 +21,25 @@ integer :: location(1) ! will store the location in the omega array of the lowes
 real, dimension(3,64) :: PES_down_ground, PES_up_ground, IPES_down_ground, IPES_up_ground
 real, dimension(3,128,2) :: LDOS
 real :: inner_product_up(64), inner_product_down(64)
+real :: IPR(128)
+integer, parameter :: nbins = 200                 ! number of bins for energy bining to get smooth curves
+real, parameter :: frequency_max = 15             ! maximum energy considered in energy bining
+real, parameter :: frequency_min = -15            ! lowest energy considered in energy bining
+real :: frequency_delta                           ! step size between different energy bins
+integer :: bin                                    ! index for the bin number the peak goes in
+real, dimension(nbins,2) :: DOS                        ! array that stores the DOS peaks and all the energy bin frequencies 
+real, dimension(nbins) :: GIPR_num, GIPR_den, GIPR     ! arrays that store the numerator and denominator and the GIPR
+real :: sum
+
+frequency_delta = (frequency_max - frequency_min)/nbins
 
 call random_gen_seed()
 call transformations()
+
+GIPR = 0
+GIPR_num = 0
+GIPR_den = 0
+DOS = 0
 
 open(unit=10,file='3citedata1.dat', status='replace', action='write',IOSTAT = error) ! open the file that output will be printed to
 if (error/=0) then
@@ -36,6 +52,8 @@ eigenvectors = 0
 E = 0
 omega_ground = 0
 omega = 0
+LDOS = 0
+IPR = 0
 
 call site_potentials(delta,E)
 call hamiltonian(E,t,U,mu)
@@ -79,23 +97,45 @@ LDOS = 0
 ! calculate the LDOS for all the cites
 do j=1,3
    do i=1,64
-      inner_product_up(i) = (dot_product(PES_up_ground(j),eigenvectors(i,:)))**2
-      inner_product_down(i) =  (dot_product(PES_down_ground(j),eigenvectors(i,:)))**2
-      LDOS(j,i,1) = omega_ground - omega(i)
-      LDOS(j,i,2) = (inner_product_up(i) + inner_product_down(i))*0.5
+      inner_product_up(i) = (dot_product(PES_up_ground(j,:),eigenvectors(i,:)))**2
+      inner_product_down(i) =  (dot_product(PES_down_ground(j,:),eigenvectors(i,:)))**2
+      LDOS(j,i,1) = omega_ground - omega(i)    ! location of the peak
+      LDOS(j,i,2) = (inner_product_up(i) + inner_product_down(i))*0.5 ! wieght of the peak
    end do
 end do
 
 do j=1,3
    do i=1,64
-      inner_product_up(i) = (dot_product(IPES_up_ground(j),eigenvectors(i,:)))**2
-      inner_product_down(i) =  (dot_product(IPES_down_ground(j),eigenvectors(i,:)))**2
-      LDOS(j,i+64,1) = omega_ground - omega(i)
-      LDOS(j,i+64,2) = (inner_product_up(i) + inner_product_down(i))*0.5
+      inner_product_up(i) = (dot_product(IPES_up_ground(j,:),eigenvectors(i,:)))**2
+      inner_product_down(i) =  (dot_product(IPES_down_ground(j,:),eigenvectors(i,:)))**2
+      LDOS(j,i+64,1) = omega(i) - omega_ground     !location of the peak
+      LDOS(j,i+64,2) = (inner_product_up(i) + inner_product_down(i))*0.5 !weight of the peak
    end do
 end do
 
+do i=1,128
+   bin = floor(LDOS(1,i,1)/frequency_delta) + nbins/2                  !find the bin number for energy bining
+   DOS(bin,2) = DOS(bin,2) + (LDOS(1,i,2) + LDOS(2,i,2) + LDOS(3,i,2))/3
+   if ((LDOS(1,i,2) + LDOS(2,i,2) + LDOS(3,i,2)) /= 0) then
+      IPR(i) = (LDOS(1,i,2)**2 + LDOS(2,i,2)**2 + LDOS(3,i,2)**2)/((LDOS(1,i,2) + LDOS(2,i,2) + LDOS(3,i,2))**2) !calculated for each transition but
+   end if !                                                                                                       not weighted with DOS
+   GIPR_num(bin) = GIPR_num(bin) + IPR(i)*(LDOS(1,i,2) + LDOS(2,i,2) + LDOS(3,i,2))/3 !numerator of the wieghted GIPR
+   GIPR_den(bin) = GIPR_den(bin) + (LDOS(1,i,2) + LDOS(2,i,2) + LDOS(3,i,2))/3  !denominator of the wieghted GIPR
+end do
+
 end do pairs
+
+sum = DOS(1,2)
+DOS(1,1) = frequency_min
+do i=2,nbins
+   DOS(i,1) = DOS(i-1,1) + frequency_delta
+   sum = sum + DOS(i,2)
+end do
+
+do i=1,nbins
+   GIPR(i) = GIPR_num(i)/GIPR_den(i)
+   write(10,*), DOS(i,1), DOS(i,2)/sum/frequency_delta
+end do
 
 close(10)
 
