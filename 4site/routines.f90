5e417cfed00207aec6b3ec09adfe2f0cca03d329
pjdaley <daleypj@mcmaster.ca>
2015-07-16 11:47:20 -0400

    changed random seed back

--- 4site/routines.f90
+++ 4site/routines.f90
@@ -24,8 +24,7 @@ subroutine random_gen_seed()
   call random_seed(size=seed_size)
   allocate(seed(seed_size))
   call system_clock(count = clock)
-  !seed=clock + 37*(/(i-1,i=1,seed_size)/)
-  seed = 3
+  seed=clock + 37*(/(i-1,i=1,seed_size)/)
   call random_seed(put=seed)
   deallocate(seed)
 
