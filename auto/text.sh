f5859dc18cf2a6ef86f316843c4e730a496a42fe
Patrick Daley <patrickdaley@Patricks-MacBook-Air.local>
2015-10-02 01:39:24 -0400

    nothing changed

--- auto/text.sh
+++ auto/text.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-nsites=4
+nsites=8
 nstates="$(echo "$((4**$nsites))")"
 
 echo "module routines
-
 	!    %-------------------------------------------------------------------------------------%
 	!    |  Fock states are represented in binary. Each one is represented with two integers:  |
 	!    |       - First integer is assosiated with up electrons                               |
@@ -15,11 +14,8 @@ echo "module routines
 	!    |  This means the fock state is (+ is up - is down and 2 is both):                    |	
 	!    |                     000-022+                                                        |
 	!    %-------------------------------------------------------------------------------------%
-
 	use lapack
-
 	implicit none
-
 	!--------------Other Parameters and Variables-------------------------------
 	integer, parameter :: nsites = $nsites             ! number of states
 	integer, parameter :: nstates = $nstates           ! size of the fock state (FS) basis
@@ -32,7 +28,6 @@ echo "module routines
 	integer, dimension(nsites,nstates) :: IPES_down, IPES_up ! lookup tables for PES of MBG transformations (ex. c^{dagger}|Psi0>)
 	integer, dimension(nsites,nstates) :: phase_PES_down, phase_PES_up    ! to get anticommutation sign right
 	integer, dimension(nsites,nstates) :: phase_IPES_down, phase_IPES_up  ! to get anticommutation sign right
-
 	!------------------Hamiltonian Submatrices---------------------------------
 	"
 for ((n_up=0; n_up<=nsites; n_up++)); do
@@ -61,82 +56,57 @@ echo "
 	integer, allocatable, dimension(:,:) :: neighbours   ! neighbours(i,:) is all the site that are nearest neighbours to site i
 	integer, dimension(0:nsites,0:nsites) :: msize       ! msize(i,j) is size of submatrix with n_up=i and n_dn=j
 	integer, dimension(0:nsites,0:nsites) :: mblock      ! mblock(i,j) is index in fock_states array of first state with n_up=i,n_dn=j
-
 contains
-
 !************************************************************************************
-
 subroutine make_filename(filename,t,U,mu,delta)
-
 	! assigns a filename for the output file based on parameters of the simulation. Naming convention found in README in the folder data
-
 	implicit none
-
 	character(len=50), intent(out) :: filename                ! output data file name
 	character(len=10) :: mu_str, t_str, U_str, W_str, s_str   ! string to hold values of parameters
 	real, intent(in) :: t,U,mu,delta                          ! values of the parameters
-
 	!--------------Convert the parameters to strings--------------------
 	write(mu_str,'(F4.1)') mu 
 	write(t_str,'(I2)') nint(t)
 	write(W_str,'(I2)') nint(delta) 
 	write(U_str,'(I2)') nint(U)
 	write(s_str,'(I1)') nsites
-
 	!-------------Contruct the file name from strings-------------------
 	write(filename,'(A)') trim(adjustl('data/a')) // trim(adjustl(s_str)) 
 	write(filename,'(A)') trim(adjustl(filename)) // '-dos+ipr_t' // trim(adjustl(t_str)) 
 	write(filename,'(A)') trim(adjustl(filename)) // 'U' // trim(adjustl(U_str)) 
 	write(filename,'(A)') trim(adjustl(filename)) // 'W' // trim(adjustl(W_str)) 
 	write(filename,'(A)') trim(adjustl(filename)) // 'mu' // trim(adjustl(mu_str)) // '.dat'
-
 end subroutine make_filename
-
 !************************************************************************************
-
 subroutine random_gen_seed()
-
 	! seeds the randome number generator (used for assigning site potentials) with system clock
-
 	implicit none
-
   	integer :: seed_size         ! the size of the random generators seed
 	integer :: clock             ! the system clock (used to seed random generator) 
 	integer :: i                 ! counter for loop
 	integer, allocatable, dimension(:) :: seed    ! will store our seed
-
 	call random_seed(size = seed_size)       ! find the size of the random generators seed
 	allocate(seed(seed_size))                ! make our seed that same size
 	call system_clock(count = clock)         ! find the system time
 	seed=clock + 37*(/(i-1,i=1,seed_size)/)  ! create our seed using the clock
 	call random_seed(put=seed)               ! seed the random generator with our seed
-
 	deallocate(seed)
-
 end subroutine random_gen_seed
-
 !************************************************************************************
-
 subroutine site_potentials(delta,E)
-
   	implicit none
-
   	real, intent(in) :: delta                ! width of the disorder (distribution from which site potentials are chosen)
   	real, intent(out) :: E(nsites)           ! the site potentials
   	real :: random                           ! random number used in intermediate step
   	integer :: i                             ! counter for loop
-
   	do i=1,nsites
      	call random_number(random)           ! gives a random number between 0 and 1
      	E(i) = delta*(random - 0.5)          ! centers the random numbers about 0
   	end do
-
 end subroutine site_potentials
 	
 !************************************************************************************
-
 subroutine num_sites()
-
 	!    %----------------------------------------------------------------------------------------------%
 	!    | Creates and orders the fock state basis. As an example the order for 2 site system is:       |
 	!    |                                                                                              |
@@ -160,27 +130,21 @@ subroutine num_sites()
 	!
 	!    It would move up by block of integers with n_up electrons (assigning them to fock_states(1,i)) then and cycle through the entire set
 	!    (assigning fock_states(2,i) while keeping n_up in same block. (See example in the box above)
-
 	implicit none
-
 	integer :: i, j, istate, isite  ! counters for loops
 	integer :: max_electrons        ! maximum amount of spin-up electrons (same as nsites)
-
 	tot_states_up = 2**nsites     ! total number of fock states with n_dn = 0
 	max_electrons = nsites 
-
 	! enter in intial variables before starting loops
 	block = 0  
     nstates_up = 0
     block(0) = 1
     nstates_up(0) = 1
-
     ! contruct the block array
 	do ne = 1, max_electrons
    		nstates_up(ne) = choose(nsites,ne)          ! number of fock states with n_up=ne (n_dn=0). Choose function defined at bottom of module
    		block(ne) = block(ne-1) + nstates_up(ne-1)  ! start of ne block is the ne-1 block plus amount of ne-1 states there are
 	end do
-
 	temp_block = block                              ! temporary array that can be modified without destroying original block array
  	
  	! %-----------------------------------------------------------------------%
@@ -190,7 +154,6 @@ subroutine num_sites()
  	! |                      |0||   1   ||      2      |     3     | 4 |      | 
  	! |                                                                       | 
  	! %-----------------------------------------------------------------------% 
-
 	do istate=0,tot_states_up-1
 		ne = 0
 		do isite=1,nsites
@@ -199,7 +162,6 @@ subroutine num_sites()
 		states_order(temp_block(ne)) = istate     ! place it at the lowest index of the block with the same n_up
 		temp_block(ne) = temp_block(ne) + 1       ! raise the lowest index of that block by 1 (don't overwrite what you just put in)
 	end do
-
 	! Uses the ordered list (states_order) created in last loop to make the fock_states
 	
 	istate = 1
@@ -212,13 +174,9 @@ subroutine num_sites()
 			end do
 		end do
 	end do
-
 end subroutine num_sites
-
 !************************************************************************************
-
 subroutine transformations()
-
 	!  %---------------------------------------------------------------------------------------------------%
 	!  |  Creates the lookup tables needed to calculate c_{i,sigma}|Psi0> and cc_{i,sigma}^{dagger}|Psi0>  |
 	!  |       - Psi0 is the many-body ground state                                                        |
@@ -238,22 +196,18 @@ subroutine transformations()
 	!  |                                                                                                   |
 	!  |  The program then uses the PES tables to calculate the IPES tables since they are opposites       |
 	!  %---------------------------------------------------------------------------------------------------%
-
 	implicit none
 	
 	integer :: i, j                           ! counters for loops
 	integer :: new_state(2)                   ! new fock state integers after the transition 
 	integer :: new_index                      ! index of the new state in the array fock_states
 	integer :: position, isite                ! both counters for loops
-
 	!------------------Zero all the variables-------------------------
 	PES_up = 0; PES_down = 0
     IPES_up = 0; IPES_down = 0
     phase_PES_up = 0; phase_PES_down = 0
     phase_IPES_up = 0; phase_IPES_down = 0
-
 	!------------------Make the PES_up tables-------------------------
-
 	do position = 1,nsites                                         ! loop over all the sites (make PES_up for PE from each site)
 		do i=1,nstates                                             ! loop over each state
 			ne = 0
@@ -283,9 +237,7 @@ subroutine transformations()
 			end if
 		end do
 	end do
-
 	!------------------Make the PES_dn tables-------------------------
-
 	do position = 1,nsites
 		do i=1,nstates
 			ne = 0
@@ -315,9 +267,7 @@ subroutine transformations()
 			end if
 		end do
 	end do
-
 	!-------Find the IPES tables----------
-
 	sites: do j=1,nsites  
      	do i=1,nstates
         	if (PES_down(j,i) /= 0) then
@@ -330,16 +280,11 @@ subroutine transformations()
         	end if
      	end do
  	end do sites
-
 end subroutine transformations
-
 !************************************************************************************
-
 subroutine make_neighbours()
-
 ! makes matrix that containes information about which sites are nearest neighbours
 ! neighbours(i,:) is a list of all the neighbours of site i. Each site has 4 nearest neighbours normally.
-
 	if (nsites == 8) then                  ! betts lattice for 8 sites
 		allocate(neighbours(nsites,4))
 		neighbours(1,1) = 8; neighbours(1,2) = 7; neighbours(1,3) = 5; neighbours(1,4) = 3
@@ -351,7 +296,6 @@ subroutine make_neighbours()
 		neighbours(7,1) = 4; neighbours(7,2) = 6; neighbours(7,3) = 1; neighbours(7,4) = 2
 		neighbours(8,1) = 6; neighbours(8,2) = 4; neighbours(8,3) = 2; neighbours(8,4) = 1
 	end if
-
 	if (nsites == 4) then                  ! linear 4 site lattice
 		allocate(neighbours(nsites,2))
 		neighbours(1,1) = 4; neighbours(1,2) = 2
@@ -359,18 +303,13 @@ subroutine make_neighbours()
 		neighbours(3,1) = 2; neighbours(3,2) = 4
 		neighbours(4,1) = 3; neighbours(4,2) = 1
 	end if
-
 	if (nsites == 2) then                 ! linear 2 site lattice
 		allocate(neighbours(nsites,1))
 		neighbours(1,1) = 2; neighbours(2,1) = 1
 	end if
-
 end subroutine make_neighbours
-
 !************************************************************************************
-
 subroutine matrix_sizes()
-
 	!  %------------------------------------------------------------------------------%
 	!  |  This subroutine makes the array matrix_sizes which contains the dimensions  |
 	!  |  of all the Hamiltonian submatrices                                          | 
@@ -378,11 +317,8 @@ subroutine matrix_sizes()
 	!  |  matrix_sizes(i,j) is the size of the matrix for FS with i up electrons      |
 	!  |  and j down electrons.                                                       |
 	!  %------------------------------------------------------------------------------%
-
 	implicit none
-
 	integer :: n_up,n_dn
-
 	do n_up=0,nsites
 		do n_dn=0,nsites
 			msize(n_up,n_dn) = choose(nsites,n_up)*choose(nsites,n_dn)
@@ -395,11 +331,8 @@ subroutine matrix_sizes()
 			end if
 		end do
 	end do
-
 end subroutine matrix_sizes
-
 !************************************************************************************
-
 subroutine make_hamiltonian2(t)
 		
 	!  %--------------------------------------------------------------------------------%	
@@ -419,7 +352,6 @@ subroutine make_hamiltonian2(t)
 	!  %--------------------------------------------------------------------------------%
 		
 	implicit none
-
 	real, intent(in) :: t                    ! the hopping integral
 	integer :: istate, isite, i, j, y        ! counters for loops
 	integer :: inbr                          ! site number of the neighbour
@@ -430,7 +362,6 @@ subroutine make_hamiltonian2(t)
 	integer :: new_index                     ! the column index of the new FS after the hopping
 	integer :: state_index                   ! the row index of the old FS before the hopping
 	integer :: n_up,n_dn                     ! the number of up ad down electrons (used to loop over each submatrix)
-
 	!------------------Zero the matrices------------------------
 	"
 
@@ -447,7 +378,6 @@ done
 
 echo "
 	call matrix_sizes()            ! contruct the array that tells the sizes of the Hamiltonian submatrices
-
 	do n_up = 0,nsites             ! loop over all submatrices
 	do n_dn = 0,nsites             ! loop over all submatrices
 	do istate = mblock(n_up,n_dn),mblock(n_up,n_dn) + msize(n_up,n_dn)-1  ! loop over all the states in each submatrix
@@ -540,13 +470,9 @@ echo "
 	end do
 	end do
 	end do
-
 end subroutine make_hamiltonian2
-
 !************************************************************************************
-
 subroutine solve_hamiltonian1(E,U,mu)
-
 	!  %--------------------------------------------------------------------------------%
 	!  | This program makes the on diagonal terms and solves the lowest eigenvalue of   |
 	!  | each submatrix.                                                                |
@@ -559,9 +485,7 @@ subroutine solve_hamiltonian1(E,U,mu)
 	!  | The final step calls LAPACK to solve for the lowest eigenvalue of each         |
 	!  | submatrix.                                                                     |
 	!  %--------------------------------------------------------------------------------%
-
 	implicit none
-
 	integer :: n_up, n_dn,i,j,isite,istate                 ! counters for loops
 	integer :: ne                                          ! counts the number of electrons
 	real, intent(in) :: E(nsites)                          ! site potentials
@@ -569,9 +493,8 @@ subroutine solve_hamiltonian1(E,U,mu)
 	real, intent(in) :: U                                  ! on-site coulomb interactions (defined at top of main.f90)
 	real, allocatable, dimension(:) :: wtemp               ! temporary matrix to hold the eigenvalues
 	real, allocatable, dimension(:,:) :: htemp, vtemp      ! temporary matrices to hold the hamiltonain and eigenvectors respectively
-
 	do n_up=0,nsites
-		do n_dn=0,nsites
+		do n_dn=n_up,nsites
 			
 			allocate(htemp(msize(n_up,n_dn),msize(n_up,n_dn)),wtemp(msize(n_up,n_dn)))  ! allocate temporary matrices of proper size
 			allocate(vtemp(msize(n_up,n_dn),msize(n_up,n_dn)))
@@ -607,21 +530,15 @@ echo "
 			deallocate(htemp,wtemp,vtemp)
 		end do
 	end do
-
 end subroutine solve_hamiltonian1
-
 !************************************************************************************
-
 subroutine solve_hamiltonian2(E,U,mu,n_up,n_dn)
-
 	!  %--------------------------------------------------------------------------------%
 	!  |  This program is identical to solve_hamiltonian1 except it only solves one     |
 	!  |  matrix instead of looping over all of them and calls a different lapack       |
 	!  |  routine which solves all the eigenvalues and eigenvectors.                    |
 	!  %--------------------------------------------------------------------------------%
-
 	implicit none
-
 	integer :: i,j,ne,isite,istate                   ! conters for loops
 	real, intent(in) :: E(nsites)                    ! site potentials
 	real, intent(in) :: mu                           ! chemical potential
@@ -667,13 +584,9 @@ echo "
    		eigenvectors(i+mblock(n_up,n_dn)-1,1:msize(n_up,n_dn)) = vtemp(1:msize(n_up,n_dn),i)  ! eigenvectors
 	end do
 	deallocate(htemp,wtemp,vtemp)
-
 end subroutine solve_hamiltonian2
-
 !************************************************************************************
-
 integer function choose(j,k)
-
     ! %---------------------------------------------%
 	! |  simple choose function from statistics:    |
 	! |                                             |
@@ -681,12 +594,10 @@ integer function choose(j,k)
 	! |       choose(i,j) =  --------               |
 	! |                      j!(i-j)!               |
 	! %---------------------------------------------%
-
     implicit none
     
     integer :: j,k,i2
     integer (kind=8) :: tj,tk,tchoose
-
     tchoose = 1
     tj = j
     tk = k
@@ -697,9 +608,6 @@ integer function choose(j,k)
        tchoose = tchoose / i2
     end do
     choose = tchoose
-
 end function choose
-
 !************************************************************************************
-
 end module"
